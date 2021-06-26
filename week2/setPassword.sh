85c87af3511b440225d02ad72d2a8cc273732c68
Thomas Davey <TRDAVEY@our.ecu.edu.au>
2021-06-26 19:39:54 +0800

    Removing old copies of week work as it now resides in the Portfolio folder

--- week2/setPassword.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Prompt ('-p') user for input using 'read' and hide their input using the silent ('-s') flag.
-read -s -p "Please enter a password:" inputPassword
-# Output an empty echo to create a new line after the read, so that the terminal doesn't become offset after the script has run.
-echo ""
-# Echo the sha256sum of the input into a text file called 'secret.txt'.
-echo $inputPassword | sha256sum > secret.txt
